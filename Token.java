117c0cd7144f472d62c3a06ddce5a4067ce92c35
Ganga L <164031403+GangaL092002@users.noreply.github.com>
2025-01-30 09:10:01 +0530

    Update Token.java

--- Token.java
+++ Token.java
@@ -1,66 +1,65 @@
-/*Write a program on datagram socket for client/server to display the
-messages on client side, typed at the server side.*/
+/*Token Bucket for congestion control*/
+/* In this the bucket is filled with constant rate called token_generation_rate untill the bucket is full */
 
-import java.net.*;
-class UDPServer {
- public static void main(String args[]) throws Exception {
- DatagramSocket serverSocket = new DatagramSocket(9876);
- System.out.println("Server is Ready for the client");
- byte[] receiveData = new byte[1024];
- byte[] sendData = new byte[1024];
- while (true) {
- DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
- serverSocket.receive(receivePacket);
- String sentence = new String(receivePacket.getData());
- System.out.println("RECEIVED: " + sentence);
- InetAddress IPAddress = receivePacket.getAddress();
- int port = receivePacket.getPort();
- String capitalizedSentence = sentence.toUpperCase();
- sendData = capitalizedSentence.getBytes();
- DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress,
-port);
- serverSocket.send(sendPacket);
+import java.util.Scanner;
+import java.util.*;
+public class Token_bucket{
+ public static void main(String args[]){
+ Scanner in = new Scanner(System.in);
+ int token_remaining=0;//total tokens in the bucket
+ int token_requested,token_sent;
+ System.out.println("Enter the bucket capacity");
+ int bucket_capacity=in.nextInt();
+ System.out.println("Enter the Token generation rate (Rate at which tokens are sent to the
+bucket)");
+ int token_gen_rate=in.nextInt();
+ System.out.println("Enter the number of Cycles the host computer sends the Tokens to the
+bucket(at constant rate)");
+ int n=in.nextInt();
+ System.out.println(String.format("%s\t%s\t%s\t%s", "Time_t", "Tokens Requested", "Tokens
+Sent", "Tokens Remaining in bucket"));
+ for(int i=0;i<n;i++){
+ token_requested=token_gen_rate;
+ if(token_requested+token_remaining>bucket_capacity){
+ token_sent=bucket_capacity-token_remaining;
+ token_remaining=bucket_capacity;
+ }
+ else{
+ token_sent=token_requested;
+ token_remaining+=token_requested;
+ }
+ System.out.println(String.format("%d\t\t%d\t\t%d\t\t%d", i + 1, token_requested, token_sent,
+token_remaining));
  }
  }
 }
-/*first run the server program and then the client program in another terminal*/
-/*UDP cleint program*/
-import java.io.*;
-import java.net.*;
-class UDPClient
-{
- public static void main(String[] args) throws Exception {
- BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
- DatagramSocket clientSocket = new DatagramSocket();
- InetAddress IPAddress = InetAddress.getByName("localhost");
- byte[] sendData = new byte[1024];
- byte[] receiveData = new byte[1024];
- System.out.println("Enter the string in lowercase so that you receive the message in Uppercase
-from the server");
- String sentence = inFromUser.readLine();
- sendData = sentence.getBytes();
- DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress,
-9876);
- clientSocket.send(sendPacket);
- DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
- clientSocket.receive(receivePacket);
- String modifiedSentence = new String(receivePacket.getData());
- System.out.println("FROM SERVER: " + modifiedSentence);
- clientSocket.close();
- }
-}
 
-/*output;-
- first run the server program in one terminal in another terminal run the client program
-Server side
-javac UDPServer.java
-java UDPServer
-Server is Ready for the client
-RECEIVED: abcdef
-Client Side
-javac UDPClient.java
-java UDPClient
-Enter the string in lowercase so that you receive the message in Uppercase from the server
-abcdef
-FROM SERVER: ABCDEF
+/*OUTPUT:-
+Enter the bucket capacity
+5
+Enter the Token generation rate (Rate at which tokens are sent to the bucket)
+2
+Enter the number of Cycles the host computer sends the Tokens to the bucket(at constant rate)
+6
+Time_t Tokens Requested Tokens Sent Tokens Remaining in bucket
+1 2 2 2
+2 2 2 4
+3 2 1 5
+4 2 0 5
+5 2 0 5
+6 2 0 5
 */
+/*OUTPUT:-
+Enter the bucket capacity
+5
+Enter the Token generation rate (Rate at which tokens are sent to the bucket)
+2
+Enter the number of Cycles the host computer sends the Tokens to the bucket(at constant rate)
+6
+Time_t Tokens Requested Tokens Sent Tokens Remaining in bucket
+1 2 2 2
+2 2 2 4
+3 2 1 5
+4 2 0 5
+5 2 0 5
+6 2 0 5
