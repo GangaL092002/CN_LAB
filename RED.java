802dde3ce71f58a8ec845308b9876c83a1819a73
Ganga L <164031403+GangaL092002@users.noreply.github.com>
2025-01-30 09:11:23 +0530

    Update RED.java

--- RED.java
+++ RED.java
@@ -1,66 +1,220 @@
-/*Write a program on datagram socket for client/server to display the
-messages on client side, typed at the server side.*/
+/*Write a program to implement random early detection (RED)
+congestion control algorithm*/
 
-import java.net.*;
-class UDPServer {
- public static void main(String args[]) throws Exception {
- DatagramSocket serverSocket = new DatagramSocket(9876);
- System.out.println("Server is Ready for the client");
- byte[] receiveData = new byte[1024];
- byte[] sendData = new byte[1024];
- while (true) {
- DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
- serverSocket.receive(receivePacket);
- String sentence = new String(receivePacket.getData());
- System.out.println("RECEIVED: " + sentence);
- InetAddress IPAddress = receivePacket.getAddress();
- int port = receivePacket.getPort();
- String capitalizedSentence = sentence.toUpperCase();
- sendData = capitalizedSentence.getBytes();
- DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress,
-port);
- serverSocket.send(sendPacket);
+import java.util.Random;
+import java.util.Scanner;
+public class RedCongestionControl {
+ public static void main(String[] args) {
+ Scanner scanner = new Scanner(System.in);
+ System.out.println("Enter the maximum number of packets:");//number of packets to be sent
+ int maxPackets = scanner.nextInt();
+ System.out.println("Enter the queue size:");//size of the queue the packets can be stored
+ int queueSize = scanner.nextInt();
+ System.out.println("Enter the maximum probability:");
+ double maxProbability = scanner.nextDouble();
+ System.out.println("Enter the minimum probability:");//used to calculated the drop probabilty
+(max-min)
+ double minProbability = scanner.nextDouble();
+ System.out.println("Enter the threshold value:");//the value after which the congestion control
+comes to action
+ int threshold = scanner.nextInt();
+ simulateCongestion(maxPackets, queueSize, maxProbability, minProbability, threshold);
+ }
+ private static void simulateCongestion(int maxPackets, int queueSize, double maxProbability,
+double minProbability, int threshold) {
+ Random rand = new Random(System.currentTimeMillis());
+ int queueLength = 0;
+ for (int i = 0; i < maxPackets; i++) {
+ double dropProbability = calculateDropProbability(queueLength, queueSize, maxProbability,
+minProbability, threshold);
+ if (queueLength >= threshold && rand.nextDouble() < dropProbability) {
+ System.out.println("Packet dropped (CONGESTION AVOIDANCE)");
+ //checking the threshold value and the probabilty to check whether to accept or reject the
+packet
+ } else {
+ System.out.println("Packet accepted " + (i + 1));
+ queueLength++;
  }
  }
-}
-/*first run the server program and then the client program in another terminal*/
-/*UDP cleint program*/
-import java.io.*;
-import java.net.*;
-class UDPClient
-{
- public static void main(String[] args) throws Exception {
- BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
- DatagramSocket clientSocket = new DatagramSocket();
- InetAddress IPAddress = InetAddress.getByName("localhost");
- byte[] sendData = new byte[1024];
- byte[] receiveData = new byte[1024];
- System.out.println("Enter the string in lowercase so that you receive the message in Uppercase
-from the server");
- String sentence = inFromUser.readLine();
- sendData = sentence.getBytes();
- DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress,
-9876);
- clientSocket.send(sendPacket);
- DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
- clientSocket.receive(receivePacket);
- String modifiedSentence = new String(receivePacket.getData());
- System.out.println("FROM SERVER: " + modifiedSentence);
- clientSocket.close();
+ }
+ private static double calculateDropProbability(int currentQueueLength, int queueSize, double
+maxProbability, double minProbability, int threshold) {
+ double slope = (maxProbability - minProbability) / (queueSize - threshold);
+ return minProbability + slope * (currentQueueLength - threshold);
  }
 }
 
-/*output;-
- first run the server program in one terminal in another terminal run the client program
-Server side
-javac UDPServer.java
-java UDPServer
-Server is Ready for the client
-RECEIVED: abcdef
-Client Side
-javac UDPClient.java
-java UDPClient
-Enter the string in lowercase so that you receive the message in Uppercase from the server
-abcdef
-FROM SERVER: ABCDEF
+/*
+output:-
+Enter the maximum number of packets:
+100
+Enter the queue size:
+20
+Enter the maximum probability:
+0.8
+Enter the minimum probability:
+0.2
+Enter the threshold value:
+10
+Packet accepted 1
+Packet accepted 2
+Packet accepted 3
+Packet accepted 4
+Packet accepted 5
+Packet accepted 6
+Packet accepted 7
+Packet accepted 8
+Packet accepted 9
+Packet accepted 10
+Packet accepted 11
+Packet accepted 12
+Packet accepted 13
+Packet accepted 14
+Packet accepted 15
+Packet accepted 16
+Packet accepted 17
+Packet dropped (CONGESTION AVOIDANCE)
+Packet accepted 19
+Packet accepted 20
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet accepted 23
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet accepted 33
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet accepted 40
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet accepted 47
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Output 2:-
+Enter the maximum number of packets:
+50
+Enter the queue size:
+10
+Enter the maximum probability:
+0.6
+Enter the minimum probability:
+0.4
+Enter the threshold value:
+5
+Packet accepted 1
+Packet accepted 2
+Packet accepted 3
+Packet accepted 4
+Packet accepted 5
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet accepted 9
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet accepted 13
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet accepted 17
+Packet dropped (CONGESTION AVOIDANCE)
+Packet accepted 19
+Packet accepted 20
+Packet accepted 21
+Packet accepted 22
+Packet accepted 23
+Packet accepted 24
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet accepted 27
+Packet dropped (CONGESTION AVOIDANCE)
+Packet accepted 29
+Packet dropped (CONGESTION AVOIDANCE)
+Packet accepted 31
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
+Packet dropped (CONGESTION AVOIDANCE)
 */
