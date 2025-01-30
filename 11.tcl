d0ddc95801ba85f7298ca28e6e8450553aeaa66d
Ganga L <164031403+GangaL092002@users.noreply.github.com>
2025-01-30 09:13:12 +0530

    Update 11.tcl

--- 11.tcl
+++ 11.tcl
@@ -1,66 +1,85 @@
-# Implement three nodes point – to – point network with duplex
-# links between them. Set the queue size, vary the bandwidth and find
-# the number of packets dropped.
+#b. Implement transmission of ping messages/trace route over a
+#network topology consisting of 6 nodes and find the number of packets
+#dropped due to congestion
 
-#Create Simulator set ns [new Simulator]
-#Open Trace file and NAM file
-set ntrace [open 10a.tr w]
+#Create Simulator
+set ns [new Simulator]
+#Use colors to differentiate the traffic
+$ns color 1 Blue
+$ns color 2 Red
+#Open trace and NAM trace file
+set ntrace [open 10b.tr w]
 $ns trace-all $ntrace
-set namfile [open 10a.nam w]
+set namfile [open 10b.nam w]
 $ns namtrace-all $namfile
 #Finish Procedure
 proc Finish {} {
 global ns ntrace namfile
-#Dump all the trace data and close the files
+#Dump all trace data and close the file
 $ns flush-trace
 close $ntrace
 close $namfile
 #Execute the nam animation file
-exec nam 10a.nam &
-#Show the number of packets dropped
-exec echo "The number of packet dropped is " &
-exec grep -c "^d" 10a.tr &
+exec nam 10b.nam &
+#Find the number of ping packets dropped
+puts "The number of ping packets dropped are "
+exec grep "^d" 10b.tr | cut -d " " -f 5 | grep -c "ping" &
 exit 0
 }
-#Create 3 nodes
-set n0 [$ns node]
-set n1 [$ns node]
-set n2 [$ns node]
-#Label the nodes
-$n0 label "TCP Source"
-$n2 label "Sink"
-#Set the color
-$ns color 1 blue
-#Create Links between nodes
-#You need to modify the bandwidth to observe the variation in packet drop
-$ns duplex-link $n0 $n1 1Mb 10ms DropTail
-$ns duplex-link $n1 $n2 1Mb 10ms DropTail
-#Make the Link Orientation
-$ns duplex-link-op $n0 $n1 orient right
-$ns duplex-link-op $n1 $n2 orient right
-#Set Queue Size
-#You can modify the queue length as well to observe the variation in packet drop
-$ns queue-limit $n0 $n1 10
-$ns queue-limit $n1 $n2 10
-#Set up a Transport layer connection.
+#Create six nodes
+for {set i 0} {$i < 6} {incr i} {
+set n($i) [$ns node]
+}
+#Connect the nodes
+for {set j 0} {$j < 5} {incr j} {
+$ns duplex-link $n($j) $n([expr ($j+1)]) 0.1Mb 10ms DropTail
+}
+#Define the recv function for the class 'Agent/Ping'
+Agent/Ping instproc recv {from rtt} {
+$self instvar node_
+puts "node [$node_ id] received ping answer from $from with round trip time $rtt
+ms"
+}
+#Create two ping agents and attach them to n(0) and n(5)
+set p0 [new Agent/Ping]
+$p0 set class_ 1
+$ns attach-agent $n(0) $p0
+set p1 [new Agent/Ping]
+$p1 set class_ 1
+$ns attach-agent $n(5) $p1
+$ns connect $p0 $p1
+#Set queue size and monitor the queue
+#Queue size is set to 2 to observe the drop in ping packets
+$ns queue-limit $n(2) $n(3) 2
+$ns duplex-link-op $n(2) $n(3) queuePos 0.5
+#Create Congestion
+#Generate a Huge CBR traffic between n(2) and n(4)
 set tcp0 [new Agent/TCP]
-$ns attach-agent $n0 $tcp0
+$tcp0 set class_ 2
+$ns attach-agent $n(2) $tcp0
 set sink0 [new Agent/TCPSink]
-$ns attach-agent $n2 $sink0
+$ns attach-agent $n(4) $sink0
 $ns connect $tcp0 $sink0
-#Set up an Application layer Traffic
+#Apply CBR traffic over TCP
 set cbr0 [new Application/Traffic/CBR]
-$cbr0 set type_ CBR
-$cbr0 set packetSize_ 100
+$cbr0 set packetSize_ 500
 $cbr0 set rate_ 1Mb
-$cbr0 set random_ false
 $cbr0 attach-agent $tcp0
-$tcp0 set class_ 1
-#Schedule Events
-$ns at 0.0 "$cbr0 start"
-$ns at 5.0 "Finish"
+#Schedule events
+$ns at 0.2 "$p0 send"
+$ns at 0.4 "$p1 send"
+$ns at 0.4 "$cbr0 start"
+$ns at 0.8 "$p0 send"
+$ns at 1.0 "$p1 send"
+$ns at 1.2 "$cbr0 stop"
+$ns at 1.4 "$p0 send"
+$ns at 1.6 "$p1 send"
+$ns at 1.8 "Finish"
 #Run the Simulation
 $ns run
 
-#output:-
-#The number of packet dropped is 8
+#output
+#node 0 received ping answer from 5 with round trip time 151.2 ms
+#node 0 received ping answer from 5 with round trip time 301.4 ms
+#node 5 received ping answer from 0 with round trip time 155.4 ms
+#The number of ping packets dropped are 3
