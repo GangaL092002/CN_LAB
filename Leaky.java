232c36d7c30c80c77da1ab96434430ee3b294084
Ganga L <164031403+GangaL092002@users.noreply.github.com>
2025-01-30 09:08:38 +0530

    Update Leaky.java

--- Leaky.java
+++ Leaky.java
@@ -1,66 +1,74 @@
-/*Write a program on datagram socket for client/server to display the
-messages on client side, typed at the server side.*/
+/*Write a program for congestion control using leaky bucket algorithm
+and token bucket algorithm.*/
+/*Leaky bucket algorithm*/
 
-import java.net.*;
-class UDPServer {
- public static void main(String args[]) throws Exception {
- DatagramSocket serverSocket = new DatagramSocket(9876);
- System.out.println("Server is Ready for the client");
- byte[] receiveData = new byte[1024];
- byte[] sendData = new byte[1024];
- while (true) {
- DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
- serverSocket.receive(receivePacket);
- String sentence = new String(receivePacket.getData());
- System.out.println("RECEIVED: " + sentence);
- InetAddress IPAddress = receivePacket.getAddress();
- int port = receivePacket.getPort();
- String capitalizedSentence = sentence.toUpperCase();
- sendData = capitalizedSentence.getBytes();
- DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress,
-port);
- serverSocket.send(sendPacket);
+import java.util.Scanner;
+public class Main {
+ public static void main(String args[]) {
+ Scanner in = new Scanner(System.in);
+ int bucket_remaining = 0, sent, received;
+ System.out.println("Enter the bucket capacity");
+ int bucket_capacity = in.nextInt();
+ System.out.println("Enter the bucket rate (Rate at which the bucket sends the packets)");
+ int bucket_rate = in.nextInt();
+ System.out.println("Enter the number of packets to be sent");
+ int n = in.nextInt();
+ int[] buf = new int[30]; // buffer array to store the packets
+ System.out.println("Enter the packets sizes one by one");
+ for (int i = 0; i < n; i++) {
+ buf[i] = in.nextInt();
+ }
+
+System.out.println(String.format("%s\t\t%s\t\t%s\t%s\t%s","Time_t","P_size","accepted","sent","rem
+aining"));
+ for (int i = 0; i < n; i++) {
+ if (buf[i] != 0) {
+ if (bucket_remaining + buf[i] > bucket_capacity) {
+ received = -1;
+ } else {
+ received = buf[i];
+ bucket_remaining += buf[i];
+ }
+ } else {
+ received = 0;
+ }
+ if (bucket_remaining != 0) {
+ if (bucket_remaining < bucket_rate) {
+ sent = bucket_remaining;
+ bucket_remaining = 0;
+ } else {
+ sent = bucket_rate;
+ bucket_remaining = bucket_remaining - bucket_rate;
+ }
+ } else {
+ sent = 0;
+ }
+ if (received == -1) {
+ System.out.println(String.format("%d\t\t%d\t\t%s\t\t%d\t\t%d", i + 1, buf[i], "dropped",
+sent, bucket_remaining));
+ } else {
+ System.out.println(String.format("%d\t\t%d\t\t%d\t\t%d\t\t%d", i + 1, buf[i], received, sent,
+bucket_remaining));
+ }
  }
  }
 }
-/*first run the server program and then the client program in another terminal*/
-/*UDP cleint program*/
-import java.io.*;
-import java.net.*;
-class UDPClient
-{
- public static void main(String[] args) throws Exception {
- BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
- DatagramSocket clientSocket = new DatagramSocket();
- InetAddress IPAddress = InetAddress.getByName("localhost");
- byte[] sendData = new byte[1024];
- byte[] receiveData = new byte[1024];
- System.out.println("Enter the string in lowercase so that you receive the message in Uppercase
-from the server");
- String sentence = inFromUser.readLine();
- sendData = sentence.getBytes();
- DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress,
-9876);
- clientSocket.send(sendPacket);
- DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
- clientSocket.receive(receivePacket);
- String modifiedSentence = new String(receivePacket.getData());
- System.out.println("FROM SERVER: " + modifiedSentence);
- clientSocket.close();
- }
-}
 
-/*output;-
- first run the server program in one terminal in another terminal run the client program
-Server side
-javac UDPServer.java
-java UDPServer
-Server is Ready for the client
-RECEIVED: abcdef
-Client Side
-javac UDPClient.java
-java UDPClient
-Enter the string in lowercase so that you receive the message in Uppercase from the server
-abcdef
-FROM SERVER: ABCDEF
+/*output:-
+ Enter the bucket capacity
+6
+Enter the bucket rate (Rate at which the bucket sends the packets)
+2
+Enter the number of packets to be sent
+4
+Enter the packets sizes one by one
+2
+3
+4
+6
+Time_t P_size accepted sent remaining
+1 2 2 2 0
+2 3 3 2 1
+3 4 4 2 3
+4 6 dropped 2 1
 */
